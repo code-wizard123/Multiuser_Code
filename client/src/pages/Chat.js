307d265ef7b6be73a0f3b1ff712fda03f64b138c
Rahul Babar <56345693+rahulbabar1212@users.noreply.github.com>
2024-02-24 13:05:25 +0530

    Chat 1.0

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,12 +1,65 @@
-import React from 'react'
-import '../css/Chat.css'
+import '../css/Chat.css';
+import React, { useState } from 'react';
 
-const Chat= () => {
-    return (
-      <div className='Chat-div'>
+const Chat = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState([]);
 
-      </div>  
-    )
-  }
-  
-  export default Chat
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Add the new input to the messages array
+    setMessages([inputValue, ...messages]);
+    // Clear the input field after submission
+    setInputValue('');
+  };
+
+  return (
+    <div className='Chat-div'>
+      <div className='Chat-display'>
+        <div className='Chat-right'>
+          <div className='Chat-btn-D'>
+            <button><img src='reject.png' alt="Reject"></img></button>
+          </div>
+        </div>
+        <div className='Chat-api'>
+          <div className='Chat-body'>
+            <div className='Chat-head'>
+              <p>Chat Window</p>
+            </div>
+            {/* Output display */}
+            <div className='Chat-Messages'>
+              {/* Render each input in a new box in reverse order */}
+              {messages.slice(0).map((message, index) => (
+                <div key={index} className='display-block'>
+                  <div className='message-bg'>
+                    <p>{message}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className='Chat-input-submit'>
+              <div className='wid-90'>
+                <input
+                  className='Chat-input'
+                  type="text"
+                  id="value"
+                  placeholder="Enter text"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                />
+                <button className='Chat-btn' onClick={handleSubmit}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
