1585ccf75d771c32a87cda89674da130b08abd13
Rahul Babar <56345693+rahulbabar1212@users.noreply.github.com>
2024-04-08 13:03:14 +0530

    Merge branch 'main' of github.com:code-wizard123/Multiuser_Code

---
+++ client/src/pages/Home_Interviewer.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import '../css/Home_Interviewer.css'
+
+
+const Home = () => {
+  return (
+    <div className='margin-auto'>
+      <div className='outer-interviewer'>
+        <div className='main-interview-A'>
+          <div className='left'>
+            <div className='text-A'>
+              <p>Stand strong in the storm, for it is the bracing winds that shape the mightiest trees</p>
+            </div>
+            <div className='text-B'>
+              <p>Welcome to our online interview hub, where opportunities meet talent. Designed to connect employers with top-notch candidates. Navigate user-friendly features, discover insightful resources, and embark on a journey of meaningful conversations. Your next career milestone awaits – dive into a world of possibilities and make your interview experience exceptional</p>
+            </div>
+            <div className='interview-submit'>
+              <button type="reset">Create Meeting</button>
+              <input type='text' className='code-submit' placeholder='Enter the Code'></input>
+            </div>
+          </div>
+          <div className='right'>
+            <img alt="img" src='Webinar-cuate.png'></img>
+          </div>
+
+        </div>
+
+      </div>
+      <div className='Details'>
+        <h3 className='h3-details'>Interviewee Details</h3>
+      </div>
+
+      <div className='scroll-details'>
+
+        <div className='card-details'>
+
+          <div className='card-A-witdh'>
+            <img src='A-bg.png' alt="A" className='A-img-width'></img>
+          </div>
+          <div className='text-width'>
+            <p className='text-A-bold'>Name : Serena Rodriguez</p>
+          </div>
+          <div className='text-width'>
+            <p className='text-bold'>Qualification: Bachelor's degree in Computer Science</p>
+          </div>
+          <div className='take-meet'>
+            <button type="reset" className='take-meet-btn'>Schedule Meeting</button>
+          </div>
+        </div>
+
+        <div className='card-details'>
+          <div className='card-A-witdh'>
+            <img src='B-bg.png' alt="A" className='A-img-width'></img>
+          </div>
+          <div className='text-width'>
+            <p className='text-A-bold'>Name : Serena Rodriguez</p>
+          </div>
+          <div className='text-width'>
+            <p className='text-bold'>Qualification: Bachelor's degree in Computer Science</p>
+          </div>
+          <div className='take-meet'>
+            <button type="reset" className='take-meet-btn'>Schedule Meeting</button>
+          </div>
+        </div>
+
+        <div className='card-details'>
+          <div className='card-A-witdh'>
+            <img src='C-bg.png' alt="A" className='A-img-width'></img>
+          </div>
+          <div className='text-width'>
+            <p className='text-A-bold'>Name : Serena Rodriguez</p>
+          </div>
+          <div className='text-width'>
+            <p className='text-bold'>Qualification: Bachelor's degree in Computer Science</p>
+          </div>
+          <div className='take-meet'>
+            <button type="reset" className='take-meet-btn'>Schedule Meeting</button>
+          </div>
+        </div>
+      </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+
+
+  )
+}
+
+export default Home
---
+++ client/src/pages/M_interviewer.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef } from 'react';
+import '../css/M_interviewer.css';
+
+const M_interviewer = () => {
+  const [isStyle, setIsStyle] = useState(true);
+  const localStream = useRef(null);
+
+  useEffect(() => {
+    const leftVideo = document.getElementById('left-video');
+
+    const init = async () => {
+      try {
+        localStream.current = await navigator.mediaDevices.getUserMedia({ video: true })
+        leftVideo.srcObject = localStream.current;
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+
+    init();
+  }, [])
+
+  const switchStyles = () => {
+    if (window.screen.width < 1200) {
+      setIsStyle(!isStyle);
+    }
+  }
+
+  const toggleCamera = () => {
+    let videoTrack = localStream.current.getTracks().find(track => track.kind === 'video')
+
+    if (videoTrack.enabled) {
+      videoTrack.enabled = false;
+    }
+    else {
+      videoTrack.enabled = true;
+    }
+  }
+
+  return (
+    <div className='meet-div'>
+      <div className='inner-div-cam'>
+
+        <div className={isStyle ? 'cam-A left-cam' : 'cam-B left-cam'} onClick={switchStyles} >
+          <video id='left-video' autoPlay></video>
+        </div>
+
+        <div className={isStyle ? 'cam-B right-cam' : 'cam-A right-cam'} onClick={switchStyles} >
+          <video id='right-video' autoPlay></video>
+        </div>
+      </div>
+
+      <div className='options'>
+        <div className='center-optn'>
+
+          <div className='btn-A'>
+            <button><img src='comment.png'></img></button>
+            <p>Chat</p>
+          </div>
+
+          <div className='btn-B'>
+            <button onClick={toggleCamera}><img src='video.png'></img></button>
+            <p>Video</p>
+          </div>
+
+          <div className='btn-C'>
+            <button><img src='code.png'></img></button>
+            <p>Code</p>
+          </div>
+
+          <div className='btn-D'>
+            <button><img src='telephoneA.png'></img></button>
+            <p>Disconnect</p>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default M_interviewer
+
+
+
+
+// <div className="scrollmenu">
+//   <a href="#home">Home</a>
+//   <a href="#news">News</a>
+//   <a href="#contact">Contact</a>
+//   <a href="#about">About</a>
+//   <a href="#support">Support</a>
+// </div>
